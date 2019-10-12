8214eb9a9fc3e73296856785505b9d5a8f9e966d
christian Vera <linuxvera@gmail.com>
2019-10-11 22:14:03 -0300

    Agregar archivos de proyecto.

---
+++ TuCredito/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TuCredito.Startup))]
+namespace TuCredito
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
