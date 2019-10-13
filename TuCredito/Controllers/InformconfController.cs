dd855ee5fa89677b58296e60d800a301368270d0
jnlopez7 <jnlopez063@gmail.com>
2019-10-13 12:36:21 -0300

    Cambio en layout, cambio en vista nosotros ABM completo de Moneda.  formulario de carga de datos Informconf solicitud de login para ambos casos

---
+++ TuCredito/Controllers/InformconfController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TuCredito.Models;
+
+namespace TuCredito.Controllers
+{
+    public class InformconfController : Controller
+    {
+        private prestamoEntities db = new prestamoEntities();
+
+        // GET: Informconf
+        public ActionResult Index()
+        {
+            return View(db.Informconf.ToList());
+        }
+
+        // GET: Informconf/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Informconf informconf = db.Informconf.Find(id);
+            if (informconf == null)
+            {
+                return HttpNotFound();
+            }
+            return View(informconf);
+        }
+
+        // GET: Informconf/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Informconf/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Documento,Nombre,Apellido,RazonSocial,Informconf1,Faja")] Informconf informconf)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Informconf.Add(informconf);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(informconf);
+        }
+
+        // GET: Informconf/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Informconf informconf = db.Informconf.Find(id);
+            if (informconf == null)
+            {
+                return HttpNotFound();
+            }
+            return View(informconf);
+        }
+
+        // POST: Informconf/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Documento,Nombre,Apellido,RazonSocial,Informconf1,Faja")] Informconf informconf)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(informconf).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(informconf);
+        }
+
+        // GET: Informconf/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Informconf informconf = db.Informconf.Find(id);
+            if (informconf == null)
+            {
+                return HttpNotFound();
+            }
+            return View(informconf);
+        }
+
+        // POST: Informconf/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Informconf informconf = db.Informconf.Find(id);
+            db.Informconf.Remove(informconf);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TuCredito/Controllers/monedaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TuCredito.Models;
+
+namespace TuCredito.Controllers
+{
+    public class monedaController : Controller
+    {
+        private prestamoEntities db = new prestamoEntities();
+
+        // GET: moneda
+        public ActionResult Index()
+        {
+            return View(db.moneda.ToList());
+        }
+
+        // GET: moneda/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            moneda moneda = db.moneda.Find(id);
+            if (moneda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moneda);
+        }
+
+        // GET: moneda/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: moneda/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "mon_codigo,mon_descripcion,mon_pais")] moneda moneda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.moneda.Add(moneda);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(moneda);
+        }
+
+        // GET: moneda/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            moneda moneda = db.moneda.Find(id);
+            if (moneda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moneda);
+        }
+
+        // POST: moneda/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "mon_codigo,mon_descripcion,mon_pais")] moneda moneda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(moneda).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(moneda);
+        }
+
+        // GET: moneda/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            moneda moneda = db.moneda.Find(id);
+            if (moneda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moneda);
+        }
+
+        // POST: moneda/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            moneda moneda = db.moneda.Find(id);
+            db.moneda.Remove(moneda);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
