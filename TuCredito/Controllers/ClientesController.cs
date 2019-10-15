a0b1daece78659b35b471920871ba13d10cc3118
Manuel González <manuel-gonzalez@outlook.com>
2019-10-15 19:34:57 -0300

    Agregada la web api para Clientes

---
+++ TuCredito/Controllers/ClientesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TuCredito.Models;
+
+namespace TuCredito.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private prestamoEntities db = new prestamoEntities();
+
+        // GET: api/Clientes
+        public IQueryable<Cliente> GetCliente()
+        {
+            return db.Cliente;
+        }
+
+        // GET: api/Clientes/5
+        [ResponseType(typeof(Cliente))]
+        public async Task<IHttpActionResult> GetCliente(int id)
+        {
+            Cliente cliente = await db.Cliente.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // PUT: api/Clientes/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCliente(int id, Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cliente.NumeroCliente)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Clientes
+        [ResponseType(typeof(Cliente))]
+        public async Task<IHttpActionResult> PostCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cliente.Add(cliente);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = cliente.NumeroCliente }, cliente);
+        }
+
+        // DELETE: api/Clientes/5
+        [ResponseType(typeof(Cliente))]
+        public async Task<IHttpActionResult> DeleteCliente(int id)
+        {
+            Cliente cliente = await db.Cliente.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            db.Cliente.Remove(cliente);
+            await db.SaveChangesAsync();
+
+            return Ok(cliente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return db.Cliente.Count(e => e.NumeroCliente == id) > 0;
+        }
+    }
+}
